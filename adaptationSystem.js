ea195f3afd75266239a16bc363d9fba531880592
aritz <aritz.sala@gmail.com>
2021-09-27 12:44:43 +0200

    appoinmentfunction improved and confirmation step added

--- adaptationSystem.js
+++ adaptationSystem.js
@@ -10,7 +10,7 @@ var eservices =
               "name": "sepe",
               "description": "",
               "url":"https://sede.sepe.gob.es/citaprevia/solicitudCitaPrevia.do",
-              "steps": ["Pregunta de seguridad","Porporcionar datos personales","Selección de servicio","Selección de fecha"],
+              "steps": ["Pregunta de seguridad","Porporcionar datos personales","Selección de servicio","Selección de fecha","Confirmación"],
 
               "pages":
                   [
@@ -30,7 +30,17 @@ var eservices =
                     [
                       {"name": "solicitudCitaPreviaCalendarioForm","type":"form","class":"name"},
                       {"name": "eguna","type":"calendar","class":"name","step":"Selección de fecha"}
+                    ],
+                    [
+                      {"name": "solicitudCitaPreviaConfirmacionForm","type":"form","class":"name"},
+                      {"name":"appoitment","type":"text","day":"dia","hour":"hora","step":"Confirmación"},
+                      {"name":"telefono","type":"input","class":"id","label":"telefono","step":"Confirmación"},
+                      {"name":"confirmarTelefono","type":"input","class":"id","label":"telefono","step":"Confirmación"},
+                      {"name":"email","type":"input","class":"id","label":"email","step":"Confirmación"},
+                      {"name":"confirmaremail","type":"input","class":"id","label":"email","step":"Confirmación"}
+
                     ]
+
                   ]
 
             }
@@ -60,7 +70,10 @@ function searchCurrentPage()
     if (eService.pages[index][0].type==="form")
       {
       if ($('form['+eService.pages[index][0].class+'=\"'+eService.pages[index][0].name+'\"]').length)
-        page=eService.pages[index];
+            {
+            page=eService.pages[index];
+            //alert(index);
+            }
       }
     if (eService.pages[index][0].type==="page")
       {
@@ -72,43 +85,6 @@ function searchCurrentPage()
   else
     return(page);
   }
-
-function navigationMenu2()
-  {
-  //Progress menu
-  currentIndexOfSteps=eService.steps.indexOf(currentStep)+1;
-  numberOfSteps=eService.steps.length;
-  $("#navigationStepProgress").empty();
-
-  $("#navigationStepProgress").append("<h3><u>Pasos del servicio</u></h3>");
-  $("#navigationStepProgress").css(   {'background-color': 'black','text-align': 'center'});
-
-  $("#title").text(currentStep);
-  var nav="";
-  var paso=0;
-  for(var index=0;index<eService.steps.indexOf(currentStep);index++)
-    {
-    paso=index+1;
-    var newElementh3 = document.createElement('h3');
-    newElementh3.id="nav1h3";
-    newElementh3.innerText=paso+" "+eService.steps[index];
-    $("#navigationStepProgress").append(newElementh3);
-    }
-  paso++;
-  var newElementh2 = document.createElement('h2');
-  newElementh2.id="navh2";
-  newElementh2.innerText=paso+" "+currentStep;
-
-  $("#navigationStepProgress").append(newElementh2);
-  for(var index=(eService.steps.indexOf(currentStep)+1);index<eService.steps.length;index++)
-    {paso++;
-    var newElementh3 = document.createElement('h3');
-    newElementh3.id="nav2h3";
-    newElementh3.innerText=paso+" "+eService.steps[index];
-    $("#navigationStepProgress").append(newElementh3);
-    }
-  }
-
 
 function navigationMenu()
   {
@@ -228,43 +204,6 @@ function hideShowPanel()
         }
     }
 
-function createPanel3()
-    {
-    panelVisible=0;
-
-    var newElementDiv = document.createElement('div');
-    newElementDiv.id = 'panel';
-    $('body').append(newElementDiv);
-
-    var newElementH1 = document.createElement('h1');
-    newElementH1.id="title";
-    panel.append(newElementH1);
-
-    var newElementH2 = document.createElement('h2');
-    newElementH2.id="subtitle";
-    panel.append(newElementH2);
-
-    var newElementP1 = document.createElement('p');
-    var newElementP2 = document.createElement('p');
-    var newElementP3 = document.createElement('p');
-
-    newElementP1.id="p1";
-    newElementP2.id="p2";
-    newElementP3.id="p3";
-
-    $('#panel').append(newElementP1);
-    $('#panel').append(newElementP2);
-    $('#panel').append(newElementP3);
-
-    var newElementNext = document.createElement('a');
-    newElementNext.id="next";
-    newElementNext.innerText=">";
-    newElementNext.className="buttonMenu";
-    panel.append(newElementNext);
-
-    $('#next').on("click",changePanel);
-    }
-
 function createPanel()
     {
     panelVisible=0;
@@ -325,6 +264,7 @@ function hide()
 
 function sendForm()
     {
+    //alert(currentStep);
     $('input[save="yes"]').each(function(index)
         {
         localStorage.setItem($(this).attr("id"), $(this).val());
@@ -335,127 +275,55 @@ function sendForm()
     $('form['+currentPage[0].class+'=\"'+currentPage[0].name+'\"]').submit();
     }
 
-function viewHours(date)
-  {
-  var newElementh1hours = document.createElement('h2');
-  newElementh1hours.innerHTML="Horas disponibles del dia X del mes Y";
-   $("#p2").remove();
-
-  var newElementp = document.createElement('p');
-  newElementp.id="p2";
-  $("#panel").append(newElementp);
-  $("#p2").append(newElementh1hours);
-
-  $('#'+date).find("a").each(function (index)
-    {
-    var hour=$(this).parent().text().trim();
-    if (!(hour===""))
-      {
-      $(this).attr('id').replace(":","");
-      var newElementA = document.createElement('a');
-      newElementA.innerHTML=hour;
-
-      newId=$(this).attr('id').replace(":","");
-
-      newElementA.id="A"+index+"-"+newId;
-      $("#p2").append(newElementA);
-      //if (((index+1) % 10) ===0) $("#p2").append("</br></br>");
-
-      //$("#"+hour).click(clickEgin);
-      $(this).removeAttr("id");
-      $(this).attr("id",newId);
-      $("#A"+index+"-"+newId).on("click",{value:$(this).attr('id')},clickHour);
-      }
-    });
-  $("p").css({'color':'white','font-size':'2em','text-align':'left'});
-  $("h2").css({'color':'white','font-size':'2em'});
-
-  $("a").css({
-  'background-color': '#f44336',
-  'color': 'white',
-  'padding': '14px 25px',
-  'text-align': 'center',
-  'text-decoration': 'none',
-  'display': 'inline-block',
-  'margin-left': '1em',
-  'margin-right': '1em',
-  'margin-top': '1em',
-  'margin-bottom': '1em',
-  });
-  }
 document.onkeydown=function(e)
-  {
-  if(e.keyCode === 27) hideShowPanel();
-  }
+    {
+    if(e.keyCode === 27) hideShowPanel();
+    }
 
-function clickHour(event)
-  {
-  document.getElementById(event.data.value).click();
-  $('input[value="Aceptar"]').trigger("click");
-  }
-
-function clickDay(event)
-  {
-  //alert(event.data.id);
-  document.getElementById(event.data.id).click();
-  viewHours("h2020-11-"+event.data.day);
-  }
-
-function clickEgin(event)
-  {
-  //alert(event.data.value);
-  //alert(event.data.ordua);
-  document.getElementById(event.data.value).click();
-  showCalendar();
-  }
 function checkEgin(event)
-  {
-  $(':radio[value="' + event.data.value + '"]').attr('checked', 'checked');
-  changePanel()
-  //sessionStorage.setItem("nombre", $('#nombre').val());
-  //sessionStorage.setItem("primerapellido",$('#apellido1').val());
-  //sessionStorage.setItem("segundoapellido",$('#apellido2').val());
-  //$('form[name="solicitudCitaPreviaForm"]').submit();
-  }
+    {
+    $(':radio[value="' + event.data.value + '"]').attr('checked', 'checked');
+    changePanel();
+    }
 
 function createListOfStep()
-  {
-  var listOfStep=[];
-  var send=true;
-  for (var indexPage=0;indexPage<eService.pages.indexOf(currentPage);indexPage++)
-     {
-    for (var indexComponent=0;indexComponent<eService.pages.length;indexComponent++)
-      {
-      if (eService.pages[indexPage][indexComponent].step===currentStep)
-        {
-        //listOfStep.push(eservice.pages[indexPage][indexComponent]);
-        }
-      }
-    }
-
-  for (var indexPage=eService.pages.indexOf(currentPage);indexPage<eService.pages.length;indexPage++)
     {
-    for (var indexComponent=0;indexComponent<eService.pages[indexPage].length;indexComponent++)
-      {
-      if (eService.pages[indexPage][indexComponent].step===currentStep)
-        {
-        listOfStep.push(eService.pages[indexPage][indexComponent]);
+    var listOfStep=[];
+    var send=true;
+    //for (var indexPage=0;indexPage<eService.pages.indexOf(currentPage);indexPage++)
+      //  {
+        //for (var indexComponent=0;indexComponent<eService.pages.length;indexComponent++)
+          //  {
+            //if (eService.pages[indexPage][indexComponent].step===currentStep)
+              //  {
+                //alert("dd");
+                //listOfStep.push(eservice.pages[indexPage][indexComponent]);
+                //}
+            //}
+        //}
 
-        if (indexPage>eService.pages.indexOf(currentPage))
-          {
-          addNewComponent(eService.pages[indexPage][indexComponent].name);
-          }
-        else
-          {
-          send = false;
-          }
+    for (var indexPage=eService.pages.indexOf(currentPage);indexPage<eService.pages.length;indexPage++)
+        {
+        for (var indexComponent=0;indexComponent<eService.pages[indexPage].length;indexComponent++)
+            {
+            if (eService.pages[indexPage][indexComponent].step===currentStep)
+                {
+                listOfStep.push(eService.pages[indexPage][indexComponent]);
+                if (indexPage>eService.pages.indexOf(currentPage))
+                    {
+                    addNewComponent(eService.pages[indexPage][indexComponent].name);
+                    }
+                 else
+                    {
+                    send = false;
+                    }
+                }
+            }
         }
-      }
+    if (send) sendForm();
+    return(listOfStep);
     }
-  //alert(currentStep);
-  if (send) sendForm();
-  return(listOfStep);
-  }
+
 function preShow()
     {
     //hideShowPanel();
@@ -469,7 +337,7 @@ function preShow()
 function show()
   {
   preShow();
-  $('#subtitle').text("");
+  $('#subtitle').empty();
 
   //#alert("show "+componentList[kont].type);
   //$('#next').show();
@@ -514,6 +382,17 @@ function show()
         $clone.appendTo('#p1').focus();
         //$('#' + componentList[kont].name).focus()
         }
+    if (componentList[kont].type==="text")
+        {
+        alert(componentList[kont].hour);
+        alert($('#'+componentList[kont].hour).text());
+        $('#p1').append($('#'+componentList[kont].day).text());
+        $('#p1').append($('#'+componentList[kont].hour).text());
+        //for (var index=currentPage;index<currentPage.length;index++)
+
+
+
+        }
     }
 function alertNotification()
   {// erroreak erakusteko
@@ -529,36 +408,60 @@ function alertNotification()
 function changePanel()
   {
   if (errorsShow==true)
-  {
-  errorsShow=false;
-  show();
-  }
-  else
-  {
-  if ($clone.val()=="")
     {
-    alert("The box wich named "+currentComponent+" is empty!")
-    }
-   else
-    {
-    $("#"+currentComponent).val($clone.val());
-    hide();
-    //alertNotification();
-    kont++;
-
-    if (componentList.length<=kont)
-        {
-        kont=0;
-        //alert(eService.steps.length);
-        //alert(eService.steps.indexOf(currentStep));
-        //if (eService.steps.length > (eService.steps.indexOf(currentStep)+1))
-        currentStep=eService.steps[eService.steps.indexOf(currentStep)+1];
-        //  alert("Click "+currentStep);
-        }
-    //alert("click"+kont);
+    errorsShow=false;
     show();
   }
-  }
+  else
+    {
+    if (typeof $clone === 'undefined')
+        {
+        //hurrengora joan
+        alert("undefined");
+
+
+            hide();
+            //alertNotification();
+            kont++;
+
+            if (componentList.length<=kont)
+                {
+                kont=0;
+                //alert(eService.steps.length);
+                //alert(eService.steps.indexOf(currentStep));
+                //if (eService.steps.length > (eService.steps.indexOf(currentStep)+1))
+                currentStep=eService.steps[eService.steps.indexOf(currentStep)+1];
+                //  alert("Click "+currentStep);
+                }
+            //alert("click"+kont);
+            show();
+
+        }
+      else
+          if ($clone.val()=="")
+            {
+            alert("The box wich named "+currentComponent+" is empty!")
+            }
+           else
+            {
+            $("#"+currentComponent).val($clone.val());
+            hide();
+            //alertNotification();
+            kont++;
+
+            if (componentList.length<=kont)
+                {
+                kont=0;
+                //alert(eService.steps.length);
+                //alert(eService.steps.indexOf(currentStep));
+                //if (eService.steps.length > (eService.steps.indexOf(currentStep)+1))
+                currentStep=eService.steps[eService.steps.indexOf(currentStep)+1];
+                //  alert("Click "+currentStep);
+                }
+            //alert("click"+kont);
+            show();
+          }
+    }
   }
 
 function createListA()
@@ -608,19 +511,24 @@ function createListAOfSelect()
 }
 function createCalendar2()
     {
+    preShow();
     var monthNum ={"Enero":"01","Febrero":"02","Marzo":"03","Abril":"04","Mayo":"05","Junio":"06","Julio":"07","Agosto":"08","Septiembre":"09","Octubre":"10","Noviembre":"11","Diciembre":"12"};
     var year,month,monthName,day;
-    //<a href="solicitudCitaPreviaCalendarioNo.do">NO</a>
+    //<a href="solicitudCitaPreviaCalendarioNo.do">NO</a> male click for showing more days
+
     if ($("a[href='solicitudCitaPreviaCalendarioNo.do']").length)
         {
         $("a[href='solicitudCitaPreviaCalendarioNo.do']").attr("id","calendarNO");
         document.getElementById("calendarNO").click();
         }
-    $("#p1").append("<h2>Meses disponibles</h2>");
-       monthYear=$("#displayCalendar th").text().split(" ");
-        monthName=monthYear[0];
-        year=monthYear[1];
-        month=monthNum[monthName];
+
+    $("#p1").append("Meses disponibles");
+    monthYear=$("#displayCalendar th").text().split(" ");
+    monthName=monthYear[0];
+    month=monthNum[monthName];
+    year=monthYear[1];
+
+
     $("#p1").append(newElementA);
     if ($('#encabezadoMeses a').length>-1)
         {
@@ -640,42 +548,36 @@ function createCalendar2()
         }
     else
         {
-        //var newElementA = document.createElement('a');
-        //newElementA.className="calendarAPP";
-        //newElementA.innerText=monthName.replaceAll(" 2021","");
-        //month=monthNum.monthName.replaceAll(" 2021","");
-        //alert("if2 "+month);
-        //  $("#p1").append(newElementA);
-
-
         var newElementA = document.createElement('a');
         newElementA.className="calendarAPP";
         newElementA.innerText=monthName;
         newElementA.id="month"+monthName;
         $("#p1").append(newElementA);
-        alert(monthName);
         $("#month"+monthName).on("click",{year:year,month:month,monthName:monthName},createDays);
 
         }
+    //update the month variable with first month available
+    monthName=monthYear[0];
+    month=monthNum[monthName];
 
     //Show days
+        $("#month"+monthName).trigger( "click",{year:year,month:month,monthName:monthName});
 
-    createDays2(year,month,monthName);
+    //createDays2(year,month,monthName);
 
     //Show hours
-   // document.getElementById(monthName).click();
     day=$('#displayCalendar a').first().text();
-    //alert(year,month,monthName,day);
-    createHours2(year,month,monthName,day);
+    $("#dayId0").trigger( "click",{year:year,month:month,monthName:monthName,day:day});
+    //createHours2(year,month,monthName,day);
     $("#next").hide();
     }
 
 function createDays(event)
     {
     document.getElementById(event.data.monthName).click();
-    alert(event.data.year+event.data.month+event.data.monthName);
+    //alert(event.data.year+event.data.month+event.data.monthName);
     $("#p2").empty();
-    $("#p2").append("<h2>Días disponibles de "+event.data.monthName+":</h2>");
+    $("#p2").append("Días disponibles de "+event.data.monthName);
     $('#displayCalendar a').each(function (index)
         {
         var newElementA = document.createElement('a');
@@ -689,28 +591,11 @@ function createDays(event)
         });
     }
 
-function createDays2(year,month,monthName)
-    {
-    $("#p2").empty();
-    $("#p2").append("<h2>Días disponibles de "+monthName+":</h2>");
-    $('#displayCalendar a').each(function (index)
-        {
-        var newElementA = document.createElement('a');
-        newElementA.className="calendarAPP";
-        newElementA.id="dayId"+index;
 
-        newElementA.innerText=$(this).text();
-        $("#p2").append(newElementA);
-        $("#dayId"+index).on("click",{year:year,month:month,monthName:monthName,day:$(this).text()},createHours);
-
-        });
-    }
 function createHours(event)
     {
     $("#p3").empty();
-    alert(event.data.year+event.data.month+event.data.monthName+event.data.day);
-
-    $("#p3").append("<h2>Horas disponibles del "+event.data.day+" de "+event.data.monthName+":</h2>");
+    $("#p3").append("Horas disponibles del "+event.data.day+" de "+event.data.monthName);
     $('#h'+event.data.year+'-'+event.data.month+'-'+event.data.day+' a').each(function (index)
         {
 
@@ -726,26 +611,15 @@ function createHours(event)
             }
         });
     }
+
+
 function selHour(event)
     {
     document.getElementById("day"+event.data.day+"month"+event.data.month+event.data.index).click();
+    //update step
+    currentStep=eService.steps[eService.steps.indexOf(currentStep)+1];
     //send the form
-    }
-function createHours2(year,month,monthName,day)
-    {
-    $("#p3").empty();
-    $("#p3").append("<h2>Horas disponibles del "+day+" de "+monthName+":</h2>");
-    $('#h'+year+'-'+month+'-'+day+' a').each(function (index)
-        {
-
-        if (($(this).text()).indexOf(":")>-1)
-            {
-            var newElementA = document.createElement('a');
-            newElementA.className = "calendarAPP";
-            newElementA.innerText = $(this).text();
-            $("#p3").append(newElementA);
-            }
-        });
+    sendForm();
     }
 function createCalendar()
     {
